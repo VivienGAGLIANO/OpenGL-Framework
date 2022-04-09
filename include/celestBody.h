4ebae26f9ee5f07eba719d4cee0f53a652e2110f
Lilian <Lilak.de.braize@gmail.com>
2022-04-09 02:21:22 -0400

    Creation of class

---
+++ include/celestBody.h
@@ -0,0 +1,23 @@
+#ifndef CELESTBODY_H
+#define CELESTBODY_H
+
+#include "object.h"
+#include <glm.hpp>
+
+class CelestBody : public Object
+{
+	public:
+		CelestBody(const std::string& name, const float m, const float r);
+		virtual void update(const double& delta_time) override;
+		virtual void resetRotation();
+		virtual void makeRotation(const double& delta_time);
+
+		float mass;
+		float radius;
+		double gradRotated;
+
+	private:
+
+};
+
+#endif
---
+++ include/planet.h
@@ -0,0 +1,20 @@
+#ifndef PLANET_H
+#define PLANET_H
+
+#include "object.h"
+#include "celestBody.h"
+#include <glm.hpp>
+
+class Planet : public CelestBody
+{
+	public:
+		Planet(const std::string& name, const float m, glm::vec3 a, glm::vec3 v, glm::vec3 p, const float r);
+		virtual void update(const double& delta_time) override;
+
+	private:
+		glm::vec3 acceleration;
+		glm::vec3 velocity;
+		glm::vec3 position;
+};
+
+#endif
