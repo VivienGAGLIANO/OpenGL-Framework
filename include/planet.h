c409358f2dc3ddc5eef22638ff16c83b080d6818
Lilian <Lilak.de.braize@gmail.com>
2022-04-13 00:06:40 -0400

    Invert class celestbody-planet parameters

--- include/planet.h
+++ include/planet.h
@@ -9,23 +9,19 @@
 class Planet : public CelestBody
 {
 	public:
-		Planet(const std::string& name, const float m, glm::vec3 v, glm::vec3 p, const float r);
+		Planet(const std::string& name, const float& m, const glm::vec3& v, const glm::vec3& p, const float& r);
 		Planet(const std::string& name, const float& m, const glm::vec3& v, const glm::vec3& p, const float& r, const glm::vec3& scale);
 		virtual void update(const double& delta_time) override;
 
-		glm::vec3 getForce();
-		glm::vec3 getVelocity();
-		glm::vec3 getPosition();
-		void resetForce();
+		virtual void resetRotation();
+		virtual void makeRotation(const double& delta_time);
+		virtual float getMass();
 		void addForce(glm::vec3 f);
-		void setVelocity(const double& delta_time);
-		void setPosition(const double& delta_time);
 
-	private:
-		glm::vec3 force;
-		glm::vec3 velocity;
-		glm::vec3 position;
-		glm::vec3 prevPosition;
+	protected:
+		float mass;
+		float radius;
+		double gradRotated;
 };
 
 #endif
