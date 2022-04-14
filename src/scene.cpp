d170e7819f35af907f9793385d5998b224517255
Lilian <Lilak.de.braize@gmail.com>
2022-04-14 00:33:44 -0400

    Added planets

--- src/scene.cpp
+++ src/scene.cpp
@@ -67,48 +67,41 @@ void Scene::populate()
 	objects.push_back(vessel);
 	nbObjects++;
 
-	// auto sun = new Planet("Sun", 100000, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), 1.f, glm::vec3// (0.05f));
-	// sun->set_material(new Material);
-	// sun->set_model(new Model("resources/model/planet/scene.gltf"));
-	// objects.push_back(sun);
-	// nbObjects++;
-	// 
-	// auto planet1 = new Planet("Planet_one", 1, glm::vec3(0.0f, 0.0f, 71.0f), glm::vec3(20.0f, 0.0f, 0.0f), 1.f, / glm::vec3/(0.01f));
-	// planet1->set_material(new Material);
-	// planet1->set_model(new Model("resources/model/planet/scene.gltf"));
-	// objects.push_back(planet1);
-	// nbObjects++;
-	// 
-	// auto planet2 = new Planet("Planet_two", 1, glm::vec3(0.0f, 0.0f, 58.0f), glm::vec3(30.0f, 0.0f, 0.0f), 1.f, / glm::vec3/(0.005f));
-	// planet2->set_material(new Material);
-	// planet2->set_model(new Model("resources/model/planet/scene.gltf"));
-	// objects.push_back(planet2);
-	// nbObjects++;
+	auto sun = new Planet("Sun", 100000, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), 1.f, glm::vec3(0.05f));
+	sun->set_material(new Material);
+	sun->set_model(new Model("resources/model/planet/scene.gltf"));
+	objects.push_back(sun);
+	nbObjects++;
 
-	// auto planet3 = new Planet("Planet_three", 1, glm::vec3(0.0f, 0.0f, 44.8f), glm::vec3(50.0f, 0.0f, 0.0f), 1.f, // glm::vec3(0.008f));
-	// planet3->set_material(new Material);
-	// planet3->set_model(new Model("resources/model/planet/scene.gltf"));
-	// objects.push_back(planet3);
-	// nbObjects++;
-	// 
-	// auto planet4 = new Planet("Planet_four", 100, glm::vec3(0.0f, 0.0f, 31.61f), glm::vec3(100.0f, 0.0f, 0.0f), 1.f, // glm::vec3(0.02f));
-	// planet4->set_material(new Material);
-	// planet4->set_model(new Model("resources/model/planet/scene.gltf"));
-	// objects.push_back(planet4);
-	// nbObjects++;
-	// 
-	// auto planet5 = new Planet("Planet_five", 0.001, glm::vec3(0.0f, 0.0f, 33.992264f), glm::vec3(107.0f,  0.0f, /0.0f), /1.f, glm::vec3(0.008f));
-	// planet5->set_material(new Material);
-	// planet5->set_model(new Model("resources/model/planet/scene.gltf"));
-	// objects.push_back(planet5);
-	// nbObjects++;
-	
-	//auto planet2= new Planet("Planet_two", 1, glm::vec3(0, 0, 0), glm::vec3(10, 0, 0), glm::vec3(0, 0, -5), 1.f);
-	//planet2->set_material(new Material);
-	//planet2->set_model(new Model("resources/model/planet/scene.gltf"));
-	//planet2->set_scale(glm::vec3(.3, .3, .3));
-	//objects.push_back(planet2);
-	//nbObjects++;
+	auto planet1 = new Planet("Planet_one", 1, glm::vec3(0.0f, 0.0f, 71.0f), glm::vec3(20.0f, 0.0f, 0.0f), 1.f, glm::vec3(0.01f));
+	planet1->set_material(new Material);
+	planet1->set_model(new Model("resources/model/planet/scene.gltf"));
+	objects.push_back(planet1);
+	nbObjects++;
+
+	auto planet2 = new Planet("Planet_two", 1, glm::vec3(0.0f, 0.0f, 58.0f), glm::vec3(30.0f, 0.0f, 0.0f), 1.f, glm::vec3(0.005f));
+	planet2->set_material(new Material);
+	planet2->set_model(new Model("resources/model/planet/scene.gltf"));
+	objects.push_back(planet2);
+	nbObjects++;
+
+	auto planet3 = new Planet("Planet_three", 1, glm::vec3(0.0f, 0.0f, 44.8f), glm::vec3(50.0f, 0.0f, 0.0f), 1.f, glm::vec3(0.008f));
+	planet3->set_material(new Material);
+	planet3->set_model(new Model("resources/model/planet/scene.gltf"));
+	objects.push_back(planet3);
+	nbObjects++;
+
+	auto planet4 = new Planet("Planet_four", 100, glm::vec3(0.0f, 0.0f, 31.61f), glm::vec3(100.0f, 0.0f, 0.0f), 1.f, glm::vec3(0.02f));
+	planet4->set_material(new Material);
+	planet4->set_model(new Model("resources/model/planet/scene.gltf"));
+	objects.push_back(planet4);
+	nbObjects++;
+
+	auto planet5 = new Planet("Planet_five", 0.001, glm::vec3(0.0f, 0.0f, 33.992264f), glm::vec3(107.0f, 0.0f, 0.0f), 1.f, glm::vec3(0.008f));
+	planet5->set_material(new Material);
+	planet5->set_model(new Model("resources/model/planet/scene.gltf"));
+	objects.push_back(planet5);
+	nbObjects++;
 }
 
 Scene::~Scene()
